6a9f733293ff2ef99185e8a6eb3426c76b56b8e9
wataru-a-72 <now.or.never34@icloud.com>
2020-05-29 13:15:34 +0900

    フロント修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
